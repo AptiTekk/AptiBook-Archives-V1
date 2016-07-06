fe6863ae169d0f339e87de873cd4fae286c89107
Mitch Talmadge <mitch@mitchtalmadge.com>
2016-07-06 15:46:58 -0600

    Jenkins pipeline improvements

--- jenkins_pipeline_stable.groovy
+++ jenkins_pipeline_stable.groovy
@@ -1,54 +1,86 @@
+mvnHome = tool "Maven";
+agendaqaUrl = "https://agendaqa-aptitekk.rhcloud.com";
+abortHandled = false;
+
 node {
-    def mvnHome = tool "Maven";
-    def agendaqaUrl = "https://agendaqa-aptitekk.rhcloud.com";
 
     try {
-        stage "Checkout Stable";
-        checkout([$class: "GitSCM", branches: [[name: "*/stable"]], browser: [$class: "GogsGit"], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: "542239bb-3d63-40bc-9cfa-e5ed56a1fc5b", url: "ssh://git@core.aptitekk.com:28/AptiTekk/Agenda.git"]]]);
+        stage "Checkout";
+        checkoutFromGit();
 
-        stage "Test Stable";
-        sh "${mvnHome}/bin/mvn clean install -P wildfly-managed -U";
+        stage "Test";
+        runTests();
         slackSend color: "good", message: "All tests for the ${env.JOB_NAME} Pipeline (Job ${env.BUILD_NUMBER}) have passed. Deploying to QA...";
 
-        stage "Build Stable WAR";
-        sh "${mvnHome}/bin/mvn clean install -P openshift -U";
+        stage "Build";
+        buildStableWAR();
 
-        stage "Deploy Stable WAR to QA";
-        sh "rhc scp agendaqa upload deployments/ROOT.war wildfly/standalone/deployments/";
+        stage "Deploy to QA";
+        deployToQA();
 
-        def i = 0;
-
-        while (i < 10) {
-            sh "curl -o /dev/null --silent --head --write-out '%{http_code}' ${agendaqaUrl} > .agendaqa-status";
-            def status = readFile ".agendaqa-status";
-
-            if (status == "200") {
-                break;
-            }
-
-            sleep 30;
-            i++;
-        }
-
-        if (i == 10) {
-            error "Could not connect to QA deployment after 5 minutes. Did it deploy?";
-        }
-
-        stage "Stable QA";
+        stage "QA Verification";
         slackSend color: "good", message: "A new QA build is ready for testing! Access it here: ${agendaqaUrl}";
+        getQAInput();
 
-        try {
-            input "Please test https://agendaqa-aptitekk.rhcloud.com/ and proceed when ready.";
-        } catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException ignored) {
+    } catch (hudson.AbortException ignored) {
+        if (!abortHandled) {
             slackSend color: "warning", message: "The ${env.JOB_NAME} Pipeline has been aborted by user. (Job ${env.BUILD_NUMBER})";
             error "Aborted by User.";
         }
-
-    } catch (hudson.AbortException ignored) {
-        slackSend color: "warning", message: "The ${env.JOB_NAME} Pipeline has been aborted by user. (Job ${env.BUILD_NUMBER})";
-        error "Aborted by User.";
     } catch (err) {
-        slackSend color: "danger", message: "An Error occurred during the ${env.JOB_NAME} Pipeline (Job ${env.BUILD_NUMBER}). Error: ${err}";
-        error err.message;
+        if (!abortHandled) {
+            slackSend color: "danger", message: "An Error occurred during the ${env.JOB_NAME} Pipeline (Job ${env.BUILD_NUMBER}). Error: ${err}";
+            error err.message;
+        }
+    }
+}
+
+def checkoutFromGit()
+{
+    def branch = "stable";
+    def url = "ssh://git@core.aptitekk.com:28/AptiTekk/Agenda.git";
+    def credentialsId = "542239bb-3d63-40bc-9cfa-e5ed56a1fc5b";
+
+    checkout([$class: "GitSCM", branches: [[name: "*/${branch}"]], browser: [$class: "GogsGit"], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: ${credentialsId}, url: ${url}]]]);
+}
+
+def runTests() {
+    sh "${mvnHome}/bin/mvn clean install -P wildfly-managed -U";
+}
+
+def buildStableWAR() {
+    sh "${mvnHome}/bin/mvn clean install -P openshift -U";
+}
+
+def deployToQA() {
+    sh "rhc scp agendaqa upload deployments/ROOT.war wildfly/standalone/deployments/";
+
+    def i = 0;
+
+    while (i < 10) {
+        sh "curl -o /dev/null --silent --head --write-out '%{http_code}' ${agendaqaUrl} > .agendaqa-status";
+        def status = readFile ".agendaqa-status";
+
+        if (status == "200") {
+            break;
+        }
+
+        sleep 30;
+        i++;
+    }
+
+    if (i == 10) {
+        error "Could not connect to QA deployment after 5 minutes. Did it deploy?";
+    }
+}
+
+def getQAInput()
+{
+    try {
+        input "Please test https://agendaqa-aptitekk.rhcloud.com/ and proceed when ready.";
+    } catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException ignored) {
+        slackSend color: "warning", message: "The ${env.JOB_NAME} Pipeline has been aborted by user. (Job ${env.BUILD_NUMBER})";
+        abortHandled = true;
+        error "Aborted by User.";
     }
 }
