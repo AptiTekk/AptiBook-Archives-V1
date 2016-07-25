99c3466a4912936480354074bce5290bab966a41
Mitch Talmadge <mitch@mitchtalmadge.com>
2016-07-25 02:28:31 -0600

    AG-70: Past Approved and Rejected reservations are now visible on their respective pages.

---
+++ src/main/java/com/aptitekk/agenda/web/controllers/reservationManagement/approved/ApprovedReservationManagementController.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2016 AptiTekk, LLC. (https://AptiTekk.com/) - All Rights Reserved
+ * Unauthorized copying of any part of Agenda, via any medium, is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+package com.aptitekk.agenda.web.controllers.reservationManagement.approved;
+
+import com.aptitekk.agenda.core.entities.AssetCategory;
+import com.aptitekk.agenda.core.entities.Reservation;
+import com.aptitekk.agenda.core.services.UserGroupService;
+import com.aptitekk.agenda.web.controllers.reservationManagement.ReservationDetails;
+import com.aptitekk.agenda.web.controllers.reservationManagement.ReservationDetailsController;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Named
+@ViewScoped
+public class ApprovedReservationManagementController implements Serializable {
+
+    @Inject
+    private ReservationDetailsController reservationDetailsController;
+
+    @Inject
+    private UserGroupService userGroupService;
+
+    private Map<AssetCategory, List<ReservationDetails>> reservationDetailsMap;
+
+    @PostConstruct
+    public void init() {
+        buildReservationList();
+    }
+
+    private void buildReservationList() {
+        this.reservationDetailsMap = reservationDetailsController.buildReservationList(Reservation.Status.APPROVED);
+    }
+
+    public List<AssetCategory> getAssetCategories() {
+        return new ArrayList<>(reservationDetailsMap.keySet());
+    }
+
+    public List<ReservationDetails> getReservationDetailsForAssetCategory(AssetCategory assetCategory) {
+        return reservationDetailsMap.get(assetCategory);
+    }
+}
--- src/main/java/com/aptitekk/agenda/web/controllers/reservationManagement/pending/PendingReservationManagementController.java
+++ src/main/java/com/aptitekk/agenda/web/controllers/reservationManagement/pending/PendingReservationManagementController.java
@@ -13,6 +13,7 @@ import com.aptitekk.agenda.core.services.UserGroupService;
 import com.aptitekk.agenda.core.util.LogManager;
 import com.aptitekk.agenda.web.controllers.AuthenticationController;
 import com.aptitekk.agenda.web.controllers.reservationManagement.ReservationDetails;
+import com.aptitekk.agenda.web.controllers.reservationManagement.ReservationDetailsController;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -31,6 +32,9 @@ public class PendingReservationManagementController implements Serializable {
     private AuthenticationController authenticationController;
 
     @Inject
+    private ReservationDetailsController reservationDetailsController;
+
+    @Inject
     private ReservationService reservationService;
 
     @Inject
@@ -39,69 +43,17 @@ public class PendingReservationManagementController implements Serializable {
     @Inject
     private UserGroupService userGroupService;
 
-    private HashMap<AssetCategory, List<ReservationDetails>> reservationDetailsMap;
+    private Map<AssetCategory, List<ReservationDetails>> reservationDetailsMap;
+
+    private ReservationDetails reservationDetails;
 
     @PostConstruct
     public void init() {
         buildReservationList();
     }
 
-    private ReservationDetails reservationDetails;
-
     private void buildReservationList() {
-        reservationDetailsMap = new LinkedHashMap<>();
-
-        Queue<UserGroup> queue = new LinkedList<>();
-        authenticationController.refreshUser();
-        queue.addAll(authenticationController.getAuthenticatedUser().getUserGroups());
-
-        //Traverse down the hierarchy and determine which reservations are pending approval.
-        //Then, build details about each reservation and store it in the reservationDetailsMap.
-        UserGroup currentGroup;
-        while ((currentGroup = queue.poll()) != null) {
-            queue.addAll(currentGroup.getChildren());
-
-            for (Asset asset : currentGroup.getAssets()) {
-                for (Reservation reservation : asset.getReservations()) {
-
-                    //Found a reservation with a pending status.
-                    if (reservation.getStatus() == Reservation.Status.PENDING) {
-
-                        //If there is not an AssetCategory already in the map, add one with an empty list.
-                        reservationDetailsMap.putIfAbsent(asset.getAssetCategory(), new ArrayList<>());
-
-                        //Traverse up the hierarchy and determine the decisions that have already been made.
-                        LinkedHashMap<UserGroup, ReservationDecision> hierarchyDecisions = new LinkedHashMap<>();
-                        List<UserGroup> hierarchyUp = userGroupService.getHierarchyUp(reservation.getAsset().getOwner());
-                        UserGroup behalfUserGroup = null;
-                        //This for loop descends to properly order the groups for display on the page.
-                        for (int i = hierarchyUp.size() - 1; i >= 0; i--) {
-                            UserGroup userGroup = hierarchyUp.get(i);
-                            //This group is the group that the authenticated user is acting on behalf of when making a decision.
-                            if (authenticationController.getAuthenticatedUser().getUserGroups().contains(userGroup))
-                                behalfUserGroup = userGroup;
-                            for (ReservationDecision decision : reservation.getDecisions()) {
-                                if (decision.getUserGroup().equals(userGroup)) {
-                                    hierarchyDecisions.put(userGroup, decision);
-                                    break;
-                                }
-                            }
-                            hierarchyDecisions.putIfAbsent(userGroup, null);
-                        }
-
-                        ReservationDecision currentDecision = null;
-                        for (ReservationDecision decision : reservation.getDecisions()) {
-                            if (decision.getUserGroup().equals(behalfUserGroup)) {
-                                currentDecision = decision;
-                                break;
-                            }
-                        }
-
-                        reservationDetailsMap.get(asset.getAssetCategory()).add(new ReservationDetails(reservation, behalfUserGroup, currentDecision, hierarchyDecisions));
-                    }
-                }
-            }
-        }
+        reservationDetailsMap = reservationDetailsController.buildReservationList(Reservation.Status.PENDING);
     }
 
     public void approveReservation(ReservationDetails reservationDetails) {
