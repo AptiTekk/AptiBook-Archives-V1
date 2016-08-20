5c27593460f6ed68a6421eb72baddc669bcdd052
Mitch Talmadge <mitch@mitchtalmadge.com>
2016-08-20 02:56:18 -0600

    AB-131: Added dependency for Google Sign-in. Added empty GoogleAuthenticationService for later use.

---
+++ src/main/java/com/aptitekk/aptibook/core/google/GoogleAuthenticationService.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2016 AptiTekk, LLC. (https://AptiTekk.com/) - All Rights Reserved
+ * Unauthorized copying of any part of AptiBook, via any medium, is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+package com.aptitekk.aptibook.core.google;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+
+@Stateless
+public class GoogleAuthenticationService {
+
+    @PostConstruct
+    private void init() {
+    }
+
+}
